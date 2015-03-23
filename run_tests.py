09d1029a27e5c3d1009f90b601d7f6e0caa45d44
Applemoon <sanyauvarov@mail.ru>
2015-03-23 22:23:49 +0300

    Болванка для задания

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python2.7
+
+import os
+import socket
+import sys
+import unittest
+from contextlib import contextmanager
+
+source_dir = os.path.join(os.path.dirname(__file__), 'source')
+sys.path.insert(0, source_dir)
+
+from MainTestCase import MainTestCase
 
 if __name__ == '__main__':
-    pass
+    suite = unittest.TestSuite((
+        unittest.makeSuite(MainTestCase),
+    ))
+    result = unittest.TextTestRunner().run(suite)
+    sys.exit(not result.wasSuccessful())
